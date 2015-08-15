002a59ac496c659fbf166674ac5365fc6c9fd41f
Belen Vicente <bvicentem@gmail.com>
2015-08-15 15:19:02 +0200

    Incluir comentarios

---
+++ models/comment.js
@@ -0,0 +1,12 @@
+// definicion de mondelo de comentario con validacion
+
+module.exports = function(sequelize, DataTypes) {
+ return sequelize.define(
+	 'Comment',
+	 { texto: {
+ 	   type: DataTypes.STRING,
+ 	   validate: { notEmpty: { msg: "-> Comentario vacío." } }
+	   }	
+	 }
+ );
+}
--- models/models.js
+++ models/models.js
@@ -30,6 +30,16 @@ var sequelize = new Sequelize( DB_name, user, pwd,
 var Quiz = sequelize.import(path.join(__dirname,'quiz'));
 exports.Quiz = Quiz;  // exportar definicion de tabla Quiz
 
+// Importar definición de la tabla Comments
+var comment_path = path.join(__dirname, 'comment');
+var Comment = sequelize.import(comment_path);
+
+Comment.belongsTo(Quiz);
+Quiz.hasMany(Comment);
+
+exports.Quiz = Quiz; // Exportar la definición de tabla Quiz
+exports.Comment = Comment;
+
 // sequelize.sync() crea e inicializa tabla de preguntas en DB
 sequelize.sync().then(function() {
 	// success(..) ejecuta el manejador una vez creada la tabla
