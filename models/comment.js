7d3db3a0241063029fdff4100883829c8a88da95
bvicentem <bvicentem@gmail.com>
2015-08-20 14:05:37 +0200

    Moderar comentarios

--- models/comment.js
+++ models/comment.js
@@ -6,7 +6,10 @@ module.exports = function(sequelize, DataTypes) {
 	 { texto: {
  	   type: DataTypes.STRING,
  	   validate: { notEmpty: { msg: "-> Comentario vacío." } }
-	   }	
+	   },
+	   publicado: {
+	   	type: DataTypes.BOOLEAN, defaultValue: false
+	   }
 	 }
  );
 }
